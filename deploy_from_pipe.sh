63f9804c9715d603dc01195dc3f411c6b400d89e
Sidnei Augusto de Miranda <sidnei.miranda@rede.local>
2023-07-26 17:39:43 -0300

    add db-borafitness.sql

--- deploy_from_pipe.sh
+++ deploy_from_pipe.sh
@@ -6,5 +6,7 @@ git pull
 sudo cp borafitness.conf /etc/apache2/sites-available/000-default.conf
 sudo cp wp-config-sample.php /var/www/html/wp-config.php
 
+
+
 # code to send static files to oracle buckets here
 #**************************************************
