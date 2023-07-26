7a3657aed4a2f9b8d5beeb7a50ece6e38b3f701f
Sidnei Augusto de Miranda <sidnei.miranda@rede.local>
2023-07-26 16:31:47 -0300

    configurado arquivo para deploy

--- deploy_from_pipe.sh
+++ deploy_from_pipe.sh
@@ -3,8 +3,8 @@
 cd ~
 cd www.borafitness.com.br
 git pull
-cp borafitness.conf /etc/apache2/sites-available/000-default.conf
-cp wp-config-sample.php /var/www/html/wp-config.php
+sudo cp borafitness.conf /etc/apache2/sites-available/000-default.conf
+sudo cp wp-config-sample.php /var/www/html/wp-config.php
 
 # code to send static files to oracle buckets here
 #**************************************************
