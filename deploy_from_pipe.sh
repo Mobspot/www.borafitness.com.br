24239f6b263bf29df7173bb67995376c810c3a73
Sidney Miranda <41812064+SidMiranda@users.noreply.github.com>
2023-07-27 20:50:19 -0300

    firt commit with wget

--- deploy_from_pipe.sh
+++ deploy_from_pipe.sh
@@ -10,21 +10,21 @@ sudo cp ~/www.borafitness.com.br/wp-config-sample.php /var/www/html/stage.borafi
 sudo chown -R www-data:www-data /var/www/html
 sudo chmod -R 775 /var/www/html
 
-# code to send static files to google buckets here
-#**************************************************
-cd ~
+# # code to send static files to google buckets here
+# #**************************************************
+# cd ~
 
-if [ ! -d "static_site" ]; then
-  sudo mkdir static_site
-else
-  sudo rm -R static_site/*
-fi
+# if [ ! -d "static_site" ]; then
+#   sudo mkdir static_site
+# else
+#   sudo rm -R static_site/*
+# fi
 
-cd ~/static_site
+# cd ~/static_site
 
-sudo wget --mirror --convert-links --adjust-extension https://stage.borafitness.com.br
-ls -la
+# sudo wget --mirror --convert-links --adjust-extension https://stage.borafitness.com.br
+# ls -la
 
-cd ~
-./google-cloud-sdk/bin/gcloud storage rm -R gs://www.borafitness.com.br/*
-./google-cloud-sdk/bin/gcloud storage cp ~/static_site/* gs://www.borafitness.com.br 
+# cd ~
+# ./google-cloud-sdk/bin/gcloud storage rm -R gs://www.borafitness.com.br/*
+# ./google-cloud-sdk/bin/gcloud storage cp ~/static_site/* gs://www.borafitness.com.br 
