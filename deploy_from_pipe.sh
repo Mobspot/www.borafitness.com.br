2d22982a6dfb6e76010e6ada71558e8f20d9e639
Sidnei Augusto de Miranda <sidnei.miranda@rede.local>
2023-07-27 07:44:41 -0300

    ajuste stage

--- deploy_from_pipe.sh
+++ deploy_from_pipe.sh
@@ -4,8 +4,11 @@ cd ~
 cd www.borafitness.com.br
 git pull
 sudo cp borafitness.conf /etc/apache2/sites-available/000-default.conf
-sudo cp wp-config-sample.php /var/www/html/wp-config.php
+sudo cp -R . /var/www/html/stage.borafitness.com.br/
+sudo cp wp-config-sample.php /var/www/html/stage.borafitness.com.br/wp-config.php
+sudo a2ensite /etc/apache2/sites-available/000-default.conf
 
+ra
 
 
 # code to send static files to oracle buckets here
