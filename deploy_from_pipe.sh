c1b4a81579edffd33b4135da403b561bb240e6b5
Sidnei Augusto de Miranda <sidnei.miranda@rede.local>
2023-07-27 15:00:20 -0300

    enviando para gcloud

--- deploy_from_pipe.sh
+++ deploy_from_pipe.sh
@@ -3,6 +3,9 @@
 sudo cp -R ~/www.borafitness.com.br/* /var/www/html/stage.borafitness.com.br/
 sudo cp wp-config-sample.php /var/www/html/stage.borafitness.com.br/wp-config.php
 
+sudo chown -R www-data:www-data /var/www/html
+sudo chomod -R 775 /var/www/html
+
 # code to send static files to oracle buckets here
 #**************************************************
 
