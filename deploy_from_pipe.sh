608f505ebc7cc970520feee3a8b96099e063111d
Sidney Miranda <41812064+SidMiranda@users.noreply.github.com>
2023-07-27 20:33:06 -0300

    firt commit with wget

--- deploy_from_pipe.sh
+++ deploy_from_pipe.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-sudo rm -R /var/www/html/stage.borafitness.com.br/*
-sudo cp -R ~/www.borafitness.com.br/* /var/www/html/stage.borafitness.com.br/
+stagePath="/var/www/html/stage.borafitness.com.br/"
+gitPath="~/www.borafitness.com.br/"
+
+sudo rm -R "$stagePath"*
+sudo cp -R "$gitPath"* /var/www/html/stage.borafitness.com.br/
 sudo cp ~/www.borafitness.com.br/wp-config-sample.php /var/www/html/stage.borafitness.com.br/wp-config.php
 
 sudo chown -R www-data:www-data /var/www/html
@@ -9,12 +12,13 @@ sudo chmod -R 775 /var/www/html
 
 # code to send static files to google buckets here
 #**************************************************
-
 cd ~
+
 if [ ! -d "static_site" ]; then
   sudo mkdir static_site
 else
   sudo rm -R static_site/*
+fi
 
 cd ~/static_site
 
