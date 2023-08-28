cdf64d23f4b372a93defcec506aa8579bb81f1b5
Sidnei Augusto de Miranda <sidnei.miranda@rede.local>
2023-08-28 17:09:41 -0300

    ajuste bora

--- deploy_from_pipe.sh
+++ deploy_from_pipe.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-sudo rm -R /var/www/html/stage.borafitness.com.br/*
-sudo cp -R ~/www.borafitness.com.br/* /var/www/html/stage.borafitness.com.br/
-sudo cp wp-config-sample.php /var/www/html/stage.borafitness.com.br/wp-config.php
+sudo rm -R /var/www/html/borafitness.com.br/*
+sudo cp -R ~/www.borafitness.com.br /var/www/html/borafitness.com.br
+sudo cp ~/www.borafitness.com.br/wp-config-sample.php /var/www/html/borafitness.com.br/wp-config.php
 
 sudo chown -R www-data:www-data /var/www/html
-sudo chomod -R 775 /var/www/html
+sudo chmod -R 775 /var/www/html
 
 # code to send static files to oracle buckets here
 #**************************************************
 
-cd ~
-./google-cloud-sdk/bin/gcloud storage rm -R gs://www.borafitness.com.br/*
-./google-cloud-sdk/bin/gcloud storage cp ~/www.borafitness.com.br/static_site/* gs://www.borafitness.com.br
+# cd ~
+# ./google-cloud-sdk/bin/gcloud storage rm -R gs://www.borafitness.com.br/*
+# ./google-cloud-sdk/bin/gcloud storage cp ~/www.borafitness.com.br/static_site/* gs://www.borafitness.com.br
