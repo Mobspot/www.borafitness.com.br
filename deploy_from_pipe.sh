f2915a281948d0f8e8e330fb2fb3124a6f5e3adb
Sidnei Augusto de Miranda <sidnei.miranda@rede.local>
2023-08-28 17:19:58 -0300

    ajuste bora

--- deploy_from_pipe.sh
+++ deploy_from_pipe.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
-sudo rm -R /var/www/html/borafitness.com.br/*
-sudo cp -R ~/www.borafitness.com.br /var/www/html/borafitness.com.br
-sudo cp ~/www.borafitness.com.br/wp-config-sample.php /var/www/html/borafitness.com.br/wp-config.php
+stagePath="/var/www/html/borafitness.com.br"
+gitPath="/home/ubuntu/www.borafitness.com.br"
 
+sudo rm -R $stagePath/*
+sudo cp -R $gitPath/* $stagePath/
+sudo cp $gitPath/wp-config-sample.php $stagePath/wp-config.php
 sudo chown -R www-data:www-data /var/www/html
 sudo chmod -R 775 /var/www/html
 
-# code to send static files to oracle buckets here
+# code to send static files to google buckets here
 #**************************************************
+# cd ~
+# if [ -d "static_site" ]; then
+#   sudo rm -R static_site/*
+# else
+#   sudo mkdir static_site
+# fi
+
+# cd ~/static_site
+# sudo wget --mirror --convert-links --adjust-extension https://stage.borafitness.com.br
 
 # cd ~
 # ./google-cloud-sdk/bin/gcloud storage rm -R gs://www.borafitness.com.br/*
-# ./google-cloud-sdk/bin/gcloud storage cp ~/www.borafitness.com.br/static_site/* gs://www.borafitness.com.br
+# ./google-cloud-sdk/bin/gcloud storage cp -R ~/static_site/stage.borafitness.com.br/* gs://www.borafitness.com.br 
